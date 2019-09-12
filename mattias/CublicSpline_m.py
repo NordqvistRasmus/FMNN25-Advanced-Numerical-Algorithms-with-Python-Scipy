8d9ac8f79dc437f22b832cb0ab37816a2058a863
Mattias Lundstrom <ma0712lu-s@esek.se>
2019-09-12 12:18:57 +0200

    m

--- mattias/CublicSpline_m.py
+++ mattias/CublicSpline_m.py
@@ -5,21 +5,47 @@ Created on %(date)s
 """
 from scipy import *
 from pylab import *
+from matplotlib.pyplot import*
 
-#class CubicSpline:
-    #__init__
-    #__call__
+class CubicSpline:
+    def __init__(self, u_vector = None):
+        if u_vector == None:
+            self.u_vector = linspace(0, 1.0, 9)
+        print("Setup input vector", self.u_vector)
+    
+    def evaluateAt(self, u, I):
+        return 99
+    
+    
+    def __call__(self, u_vector):
+            u_vector.sort()
+            
+            result = empty(len(u_vector))
+        
+            for i,u in enumerate(u_vector):
+                I = (u < u_vector).argmax()
+                print(I)
+                result[i] = self.evaluateAt(u, I)
+            
 
-    #def plot:
+    def plot():
+        return 0
+
+input = array([0.1, 0.3, 0.6, 0.9])
+Spline = CubicSpline()
+res = Spline(input)
+
+
+
 
 # @index, 
-    
-u_vector = array([1., 2. ,4. ,5.5 ,8 ])
+""" 
 
+u_vector = array([1., 2. ,4. ,5.5 ,8 ])
+N = array([])
 
 def createBasisFunction(j, u): 
-    return 0
-    
+    return 0    
 
 def getBasisFirstDegree(j, u):
         if u_vector[j-1] == u_vector[j]:
@@ -29,8 +55,6 @@ def getBasisFirstDegree(j, u):
         else:
             return 0
 
-    
-u = array([1., 2. ,4. ,5.5 ,8 ])
-getBasisFirstDegree(1, 4)
-    
-    
+"""  
+
+
