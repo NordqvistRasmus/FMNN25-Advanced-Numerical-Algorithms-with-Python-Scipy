cbc10729d568973d01921ce78b7027247beb0041
Johan Liljegren <johanliljegren@Johans-MacBook-Air-2.local>
2019-09-12 09:46:35 +0200

    test

---
+++ johan/cubicspline.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 11:38:09 2019
+
+@author: johanliljegren
+"""
+from  scipy import *
+from  pylab import *
+
+class CubicSpline:
+    def __init__(self):
+        self.A = A
+        
+    def __call__(self):
+        pass
+        
+    def deBoor(knotseq, index):
+        a = 3.141592
+        return a
+    
+    def plot():
+        pass
+    
+
+    
