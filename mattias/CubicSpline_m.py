291571efba60e5d46988dfccddfdfd097f87f2f7
Mattias Lundstrom <ma0712lu-s@esek.se>
2019-09-12 19:39:22 +0200

    närmar sig

--- mattias/CubicSpline_m.py
+++ mattias/CubicSpline_m.py
@@ -9,13 +9,17 @@ from matplotlib import pyplot as plt
 
 class CubicSpline:
     """
-    Input: Control points and u grid (optional, otherwise default grid)
+    Input: Control points and kont grid (optional, otherwise default grid)
     """
-    def __init__(self, cp, u_vector = None):
+    def __init__(self, cp, knots = None):
         self.cp = cp
-        if u_vector is None:
-            self.u_vector = linspace(0,10,len(cp) - 2) #default grid, two more points than the control points
-            print("No input grid, set default:", self.u_vector)
+        if knots is None:
+            self.knots = linspace(0,10,len(cp) - 2) #default grid, two more points than the control points
+            print("No input grid, set default:", self.knots)
+        self.knots.sort()
+        print("Knots:", self.knots)
+        self.u_vector = self.addClamp(self.knots)
+        print("Clamped knots:", self.knots)
         print("Control point vector:", cp)
     
     """
@@ -24,23 +28,21 @@ class CubicSpline:
     
 
     
-    def addClamp(self, u_vector):
-        return r_[u_vector[0], u_vector[0], u_vector, u_vector[-1], u_vector[-1]]
+    def addClamp(self, knots):
+        return r_[knots[0], knots[0], knots, knots[-1], knots[-1]]
 
     
     
     """
     Sort, add clamp (padding), and evaluate every u in u_vector
+    num - number of u to evaluate, increases precision. 
     """
-    def __call__(self): #grid vector as parameter?
-        self.u_vector.sort() 
-        print("Grid:", self.u_vector)
-        self.u_vector = self.addClamp(self.u_vector)
-        print("Clamped:", self.u_vector)
-        result = empty(len(self.u_vector)) #prep for input
+    def __call__(self, num): #grid vector as parameter?
+        u_vector = linspace(self.knots[0], self.knots[-1], num)
+        result = empty(len(u_vector)) #prep for input
     
-        for i,u in enumerate(self.u_vector): #input count, element 
-            I = (u < self.u_vector).argmax()
+        for i,u in enumerate(u_vector): #input count, element 
+            I = (u < self.knots).argmax()
             print("Count:" , i, " Hot Index:" , I, "Value: ", u)
             result[i] = self.evaluateAt(u, I) #store result in resultvector
         return result
