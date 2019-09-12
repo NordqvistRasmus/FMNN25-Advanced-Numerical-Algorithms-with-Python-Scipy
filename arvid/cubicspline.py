058239181885be093fad9492512e8c5392fc8fdd
Arvid Rolander <Arvid@Arvids-MacBook-Air.local>
2019-09-12 12:37:03 +0200

    Added skeleton class and started on point_eval method

--- arvid/cubicspline.py
+++ arvid/cubicspline.py
@@ -6,4 +6,28 @@ Created on Wed Sep 11 14:42:55 2019
 @author: Arvid
 """
 from scipy import *
-from pylab import *
+from matplotlib.pyplot import *
+
+class CubicSpline:
+    
+    
+    def __init__(self, grid_u, control_d):
+        self.grid_u = grid_u
+        self.control_d = control_d
+        
+    def __call__(self,):
+        pass
+    
+    def plot(self, plot_poly = False):
+        pass
+    
+    def point_eval(self, u):
+        if u < self.grid_u[0] or u > self.grid_u[-1]:
+            raise ValueError(f"""u = {u} is not contained in
+                             the grid [{self.grid_u[0]}, {self.grid_u[-1]}]
+                             """)
+        indx = (u > self.grid_u).argmax()
+        ctrl_points = array([self.control_d[:,i] for i in range(indx-1, indx+2)])
+        
+
+
