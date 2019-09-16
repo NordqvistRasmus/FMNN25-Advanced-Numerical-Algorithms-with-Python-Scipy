5968eb9e2693abfacc8ea425bcbd33ebf51ad2c2
Arvid Rolander <Arvid@Arvids-MacBook-Air.local>
2019-09-16 10:54:48 +0200

    FIxed a bug in basis_function method

--- arvid/cubicspline.py
+++ arvid/cubicspline.py
@@ -50,10 +50,11 @@ class CubicSpline:
         blossoms = alphas*blossoms[0] + (1-alphas)*blossoms[1]
         
         return blossoms
-<<<<<<< HEAD
+
     
     def basis_function(self, knots, i):
-        def basis(u,i, k):
+        
+        def basis(u, i, k):
             if k == 0:
                 if knots[i-1] == knots[i]:
                     return 0
@@ -62,11 +63,11 @@ class CubicSpline:
                 else:
                     return 0
             else:
-                if knots[i+k-1] == knots[i-1] and u == knots[i-1]:
+                if knots[i+k-1] == knots[i-1]: # and u == knots[i-1]:
                     coeff1 = 0
                 else:
                     coeff1 = (u - knots[i-1])/(knots[i+k-1]-knots[i-1])
-                if knots[i+k] == knots[i] and u == knots[i+k]:
+                if knots[i+k] == knots[i]: # and u == knots[i+k]:
                     coeff2 = 0
                 else:
                     coeff2 = (knots[i+k] - u)/(knots[i+k] - knots[i])
@@ -76,41 +77,7 @@ class CubicSpline:
             return basis(u, i, 3)
             
         return j_basis
-                
-            
-            
-=======
->>>>>>> c6e8e6fdb063efc6dd40ce15c00e0c9374d038d7
-        
-KNOTS = array([0.  , 0.  , 0.  , 0.12, 0.16, 0.2 , 0.24, 0.28, 0.32, 0.36, 0.4 ,
-       0.44, 0.48, 0.52, 0.56, 0.6 , 0.64, 0.68, 0.72, 0.76, 0.8 , 0.84,
-       0.88, 1.  , 1.  , 1.  ])
-    
- 
-CONTROL = [(-12.73564, 9.03455),
- (-26.77725, 15.89208),
- (-42.12487, 20.57261),
- (-15.34799, 4.57169),
- (-31.72987, 6.85753),
- (-49.14568, 6.85754),
- (-38.09753, -1e-05),
- (-67.92234, -11.10268),
- (-89.47453, -33.30804),
- (-21.44344, -22.31416),
- (-32.16513, -53.33632),
- (-32.16511, -93.06657),
- (-2e-05, -39.83887),
- (10.72167, -70.86103),
- (32.16511, -93.06658),
- (21.55219, -22.31397),
- (51.377, -33.47106),
- (89.47453, -33.47131),
- (15.89191, 0.00025),
- (30.9676, 1.95954),
- (45.22709, 5.87789),
- (14.36797, 3.91883),
- (27.59321, 9.68786),
- (39.67575, 17.30712)]
+
 
 if __name__ == '__main__':
     c = CubicSpline(KNOTS, CONTROL)
