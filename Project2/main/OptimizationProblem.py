bb70440a3410b29a5ed1567ddd3379ee6d17207c
Pontus Nordqvist <p.nordq@gmail.com>
2019-09-26 11:04:12 +0200

    Added to main

---
+++ Project2/main/OptimizationProblem.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 21:31:59 2019
+
+@author: johanliljegren
+"""
+from  scipy import *
+from  pylab import *
+
+class  optimizationProblem():
+    
+    def __init__ (objective_function, x_0, gradient = None, hessian = None):
+        pass
+    
+    def __call__(self):
+        """
+        Might be redundant
+        """
+        pass
