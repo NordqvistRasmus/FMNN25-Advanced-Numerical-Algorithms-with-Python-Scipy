b9ab61ab5f892fd1ac394e26b28261b4c175b4db
Mattias Lundstrom <ma0712lu-s@esek.se>
2019-10-11 12:07:54 +0200

    ee

--- Project3/mattias/roomHeat.py
+++ Project3/mattias/roomHeat.py
@@ -11,7 +11,7 @@ from scipy.linalg import block_diag, solve
 
 
 
-dx = 1/5
+dx = 1/6
 n = int(1/dx)
 BC_normal = 15
 BC_heater = 40
@@ -23,7 +23,7 @@ BC_window = 5
 
 A1 = diag(-4*ones(n-1)) + diag(ones(n-2), 1) + diag(ones(n-2), -1) 
 A = block_diag(A1, A1) #First block
-for i in range(2,2*n-1): # All other building blocks (to 2n-1 for 2x2 block)
+for i in range(2,2*n-1): # All other building blocks (to 2n-1 for 2x1 block)
     A = block_diag(A, A1)
 
 A = A + diag(ones(A.shape[0]-(n-1)), n-1) +  diag(ones(A.shape[0]-(n-1)), -(n-1))
@@ -31,18 +31,20 @@ N = A.shape[0] # Size of A
 
 # Create arrays of boundry conditions. North, West, East, South
 BC_N = array([[*BC_heater*ones(n-1), *zeros(N - (n-1))]]).T #OK
-
 BC_S = array([[*zeros(N - (n-1)), *BC_window*ones(n-1)]]).T #OK
 
-BC_W = array([zeros(N)]).T  #OK
-for i in range(0, int(np.ceil(N/2)), n-1):
+BC_W = 25*array([zeros(N)]).T  #OK
+for i in range(0, int(np.ceil(N/2)), n-1): #Sets interface also to BC_normal temperature
     BC_W[i] = BC_normal
 
 BC_E = array([zeros(N)]).T  #OK
-for i in range(N-1, int(np.ceil(N/2)), -(n-1)):
+for i in range(N-1, int(np.ceil(N/2)), -(n-1)): #Sets interface also to BC_normal temperature
     BC_E[i] = BC_normal
 
 # Collect dirichlet boundry conditions in b, then solve Au = b
 b = - BC_N - BC_S - BC_W - BC_E
+A = A/(dx**2) 
+b = b/(dx**2)
 u = solve(A, b)
-print(u)
+#print()
+print(u.reshape(2*n-1, n-1)) #For 2x1 room inner points
---
+++ Project3/mattias/roomHeatSolver.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 20:16:48 2019
+@author: Mattias Lundström1
+"""
+from mpi4py import MPI
+from numpy import diag, ones, zeros, array
+import numpy as np
+from scipy.linalg import block_diag, solve
+import problem as problem
+
+class roomHeatSolver:
+    
+    def __init__(self, problem):
+        self.problem = problem
+        self.dx = problem.dx
+        self.BC_normal = problem.wall
+        self.BC_heater = problem.BC_heater
+        self.BC_window = problem.window
+        self.n = int(1/self.dx)
+
+    def solveLargeRoom(self, n):
+        A1 = diag(-4*ones(n-1)) + diag(ones(n-2), 1) + diag(ones(n-2), -1) 
+        A = block_diag(A1, A1) #First block
+        for i in range(2,2*n-1): # All other building blocks (to 2n-1 for 2x1 block)
+        A = block_diag(A, A1)
+
+        A = A + diag(ones(A.shape[0]-(n-1)), n-1) +  diag(ones(A.shape[0]-(n-1)), -(n-1))
+        N = A.shape[0] # Size of A
+
+        # Create arrays of boundry conditions. North, West, East, South
+        BC_N = array([[*BC_heater*ones(n-1), *zeros(N - (n-1))]]).T #OK
+        BC_S = array([[*zeros(N - (n-1)), *BC_window*ones(n-1)]]).T #OK
+
+        BC_W = 25*array([zeros(N)]).T  #OK
+        for i in range(0, int(np.ceil(N/2)), n-1): #Sets interface also to BC_normal temperature
+            BC_W[i] = BC_normal
+
+        BC_E = array([zeros(N)]).T  #OK
+        for i in range(N-1, int(np.ceil(N/2)), -(n-1)): #Sets interface also to BC_normal temperature
+            BC_E[i] = BC_normal
+
+        # Collect dirichlet boundry conditions in b, then solve Au = b
+        b = - BC_N - BC_S - BC_W - BC_E
+        A = A/(dx**2) 
+        b = b/(dx**2)
+        u = solve(A, b)
+        #print()
+
+
+
+
+
+
+
