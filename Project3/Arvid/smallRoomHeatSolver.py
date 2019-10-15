6409298a44bb0bda5e38d90d913deaf37efdcebe
Arvid Rolander <arvid.rolander@gmail.com>
2019-10-15 15:17:29 +0200

    Fixed small solver

--- Project3/Arvid/smallRoomHeatSolver.py
+++ Project3/Arvid/smallRoomHeatSolver.py
@@ -50,14 +50,14 @@ class smallRoomHeatSolver():
         BC_S[-1,0:] = self.normal_wall
         
         if self.interface_dir == 'west':
-            BC_E[:,-1] = self.heater
-            BC_W[:,0] = self.interface_vals
+            BC_E[:,-1] = self.heater/(self.dx**2)
+            BC_W[:,0] = self.interface_vals/self.dx
             neumann_ind = nonzero(BC_W.reshape(self.N_elements))
         elif self.interface_dir == 'east':
-            BC_E[:,-1] = self.interface_vals
-            BC_W[:,0] = self.heater
+            BC_E[:,-1] = self.interface_vals/self.dx
+            BC_W[:,0] = self.heater/self.dx**2
             neumann_ind = nonzero(BC_E.reshape(self.N_elements))
-        BC_tot = BC_W + BC_E + BC_N + BC_S
+        BC_tot = BC_W + BC_E + BC_N/self.dx**2 + BC_S/self.dx**2
         
         BC_tot = BC_tot.reshape(self.size[0]*self.size[1])
         return BC_tot, neumann_ind
@@ -94,16 +94,17 @@ class smallRoomHeatSolver():
         return u, interface_vals
         
     def getMatrix(self):
-        room = zeros(self.n_rows+2, self.n_cols+1)
+        room = zeros((self.n_rows+2, self.n_cols+1))
         if self.interface_dir == 'east':
-            room[1:-2,1:-1] = self.solution.reshape(self.size)
+            room[1:-1,1:] = self.solution.reshape(self.size)
             room[0, :] = self.normal_wall*ones(self.n_cols+1)
             room[:, 0] = self.heater*ones(self.n_rows+2)
             room[-1, :] = self.normal_wall*ones(self.n_cols+1) 
         elif self.interface_dir == 'west':
-            room[1:-2, 0:-2] = self.solution.reshape(self.size)
+            room[1:-1, 0:-1] = self.solution.reshape(self.size)
             room[0,:] = room[-1,:] = self.normal_wall*ones(self.n_cols+1)
             room[-1, :] = self.heater*ones(self.n_rows+2)
+        print('Complete room is: {}'.format(room))
         return room
     
     
